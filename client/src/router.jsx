c445ab548b4a38bcf30621d604f3b0969e408d34
kanji2001 <kanjisonagara2001@gmail.com>
2025-09-19 11:03:17 +0530

    made thing perfect

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import Requirements from './pages/Requirements'
+import ResetPassword from './pages/ResetPassword'
 
 export default function App({ Protected }) {
   return (
@@ -9,6 +10,7 @@ export default function App({ Protected }) {
       <Route path="/" element={<Home />} />
       <Route path="/dashboard" element={<Protected><Dashboard /></Protected>} />
       <Route path="/requirements" element={<Protected><Requirements /></Protected>} />
+      <Route path="/reset-password" element={<ResetPassword />} />
     </Routes>
   )
 }
